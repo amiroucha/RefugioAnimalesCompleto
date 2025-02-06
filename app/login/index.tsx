3df4e7115daddd91adc7f1ef0cb46fb73da0c981
DESKTOP-7QQ3K3B\Moni <monica_97_ct@hotmail.com>
2025-02-06 13:48:26 +0100

    scrollview arreglado de login

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -7,66 +7,68 @@ import { router } from "expo-router";
 
 const login = () => {
   return (
-    <ScrollView contentContainerStyle={[GlobalStyles.contenedor, GlobalStyles.box]}>
-  
-      <View style={{ height: 100, width: '100%', position: 'absolute', top: 13, alignItems: 'flex-end' }}>
-        <Image 
-          source={require("../../assets/images/logo_2.png")}
-          style={{ width: 60, height: 60, marginRight: 20 }}
-        />
-      </View>
-
-      <View style={{ position: 'relative', top: -10 }}>
-        <Text style={GlobalStyles.tituloLogin}>Bienvenido a</Text>
-        <Text style={GlobalStyles.PetConnect}>PetConnect</Text>
-      </View>
-
-      <View style={GlobalStyles.fondoBlancoLog}>
-        <View style={{ marginLeft: 20, marginRight: 20 }}>
+    <ScrollView>
+    
+      <View style={[GlobalStyles.contenedor, GlobalStyles.box]}>
+        
+        <View style={{ height: 100, width: '100%', position: 'absolute', top: 13, alignItems: 'flex-end' }}>
           <Image 
-            source={require("../../assets/images/perroGatoJuntos.png")}
-            style={GlobalStyles.perroGato}
+            source={require("../../assets/images/logo_2.png")}
+            style={{ width: 60, height: 60, marginRight: 20 }}
           />
+        </View>
 
-          <View style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', bottom: 50, alignItems: 'center' }}>
+        <View style={{ position: 'relative', top: -10 }}>
+          <Text style={GlobalStyles.tituloLogin}>Bienvenido a</Text>
+          <Text style={GlobalStyles.PetConnect}>PetConnect</Text>
+        </View>
+
+        <View style={GlobalStyles.fondoBlancoLog}>
+          <View style={{ marginLeft: 20, marginRight: 20 }}>
             <Image 
-              source={require("../../assets/images/huellaNaranja.png")}
-              style={GlobalStyles.pataArriba}
-            />
-            <Text style={GlobalStyles.textoAccede}>Accede al refugio</Text>
-            <Image 
-              source={require("../../assets/images/huellaNaranja.png")}
-              style={GlobalStyles.pataAbajo}
+              source={require("../../assets/images/perroGatoJuntos.png")}
+              style={GlobalStyles.perroGato}
             />
 
-            <TextInput
-              style={GlobalStyles.input}
-              placeholder="Email"
-            />
+            <View style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', bottom: 50, alignItems: 'center' }}>
+              <Image 
+                source={require("../../assets/images/huellaNaranja.png")}
+                style={GlobalStyles.pataArriba}
+              />
+              <Text style={GlobalStyles.textoAccede}>Accede al refugio</Text>
+              <Image 
+                source={require("../../assets/images/huellaNaranja.png")}
+                style={GlobalStyles.pataAbajo}
+              />
 
-            <TextInput
-              style={GlobalStyles.input}
-              placeholder="Contraseña"
-              secureTextEntry={true}
-            />
+              <TextInput
+                style={GlobalStyles.input}
+                placeholder="Email"
+              />
 
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}>
-              <Pressable onPress={() => { router.push('../signUp') }}>
-                <Text style={GlobalStyles.textRegistrar}>Regístrate aquí</Text>
-              </Pressable>
+              <TextInput
+                style={GlobalStyles.input}
+                placeholder="Contraseña"
+                secureTextEntry={true}
+              />
 
-              <Boton label="Acceder" backgroundColor="naranja" link="../drawer" />
+              <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}>
+                <Pressable onPress={() => { router.push('../signUp') }}>
+                  <Text style={GlobalStyles.textRegistrar}>Regístrate aquí</Text>
+                </Pressable>
+
+                <Boton label="Acceder" backgroundColor="naranja" link="../drawer" />
+              </View>
             </View>
           </View>
         </View>
-      </View>
 
-      <View>
-        <Text style={GlobalStyles.olvidarContra}>¿Olvidaste la contraseña?</Text>
-      </View>
+        <View>
+          <Text style={GlobalStyles.olvidarContra}>¿Olvidaste la contraseña?</Text>
+        </View>
 
+      </View>
     </ScrollView>
-
   )
 }
 
